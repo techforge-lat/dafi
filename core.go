49729579748c8e4cdf827acf28a432b24e716b79
hernan <hernan_rm@outlook.es>
2024-11-15 07:26:34 -0600

    feat: adds natural language functions to create filters

--- core.go
+++ core.go
@@ -20,3 +20,51 @@ type Criteria struct {
 func New() Criteria {
 	return Criteria{}
 }
+
+func Where(field string, operator FilterOperator, value any) Criteria {
+	return Criteria{
+		Filters: FilterBy(field, operator, value),
+	}
+}
+
+func (c Criteria) AndGroup(filters ...Filter) Criteria {
+	c.Filters = c.Filters.AndGroup(filters...)
+
+	return c
+}
+
+func (c Criteria) OrGroup(filters ...Filter) Criteria {
+	c.Filters = c.Filters.OrGroup(filters...)
+
+	return c
+}
+
+func (c Criteria) Or(field string, operator FilterOperator, value any) Criteria {
+	c.Filters = c.Filters.Or(field, operator, value)
+
+	return c
+}
+
+func (c Criteria) And(field string, operator FilterOperator, value any) Criteria {
+	c.Filters = c.Filters.And(field, operator, value)
+
+	return c
+}
+
+func (c Criteria) SortBy(field string, sortType SortType) Criteria {
+	c.Sorts = append(c.Sorts, Sort{Field: SortBy(field), Type: sortType})
+
+	return c
+}
+
+func (c Criteria) Limit(value uint) Criteria {
+	c.Pagination.PageSize = value
+
+	return c
+}
+
+func (c Criteria) Page(value uint) Criteria {
+	c.Pagination.PageNumber = value
+
+	return c
+}
--- filter.go
+++ filter.go
@@ -46,3 +46,79 @@ type Filters []Filter
 func (f Filters) IsZero() bool {
 	return len(f) == 0
 }
+
+func FilterBy(name string, operator FilterOperator, value any) Filters {
+	return Filters{
+		{
+			Field:    FilterField(name),
+			Operator: operator,
+			Value:    value,
+		},
+	}
+}
+
+func (f Filters) Or(field string, operator FilterOperator, value any) Filters {
+	if f.IsZero() {
+		return Filters{{Field: FilterField(field), Operator: operator, Value: value}}
+	}
+
+	if !f.IsZero() {
+		f[len(f)-1].ChainingKey = Or
+	}
+
+	return append(f, Filter{
+		Field:    FilterField(field),
+		Operator: operator,
+		Value:    value,
+	})
+}
+
+func (f Filters) And(field string, operator FilterOperator, value any) Filters {
+	if f.IsZero() {
+		return Filters{{Field: FilterField(field), Operator: operator, Value: value}}
+	}
+
+	if !f.IsZero() {
+		f[len(f)-1].ChainingKey = And
+	}
+
+	return append(f, Filter{
+		Field:    FilterField(field),
+		Operator: operator,
+		Value:    value,
+	})
+}
+
+func (f Filters) AndGroup(filters ...Filter) Filters {
+	if len(filters) == 0 {
+		return f
+	}
+
+	if len(f) > 0 {
+		f[len(f)-1].ChainingKey = And
+	}
+
+	filters[0].IsGroupOpen = true
+	filters[len(filters)-1].IsGroupClose = true
+
+	f = append(f, filters...)
+
+	return f
+}
+
+func (f Filters) OrGroup(filters ...Filter) Filters {
+	if len(filters) == 0 {
+		return f
+	}
+
+	if len(f) > 0 {
+		f[len(f)-1].ChainingKey = Or
+	}
+
+	filters[0].IsGroupOpen = true
+	filters[len(filters)-1].IsGroupClose = true
+
+	f = append(f, filters...)
+
+	return f
+}
